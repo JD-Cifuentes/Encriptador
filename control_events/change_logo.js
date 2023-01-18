1ace05fac874327332936ba33f2e2454752ceb5c
Juan David Cifuentes Florez <juan-12_91@hotmail.com>
2023-01-18 16:57:22 -0500

    logo image test

--- control_events/change_logo.js
+++ control_events/change_logo.js
@@ -1,19 +1,20 @@
-const RELOAD_LOGO = document.getElementById("logo");
-if(RELOAD_LOGO){
-    logoSize();
-}
 
-function logoSize() {
-    if(window.screen.width <= 768 && window.screen.width > 375){
-        RELOAD_LOGO.src = "../img/logo_tab_1x.png";    
-        return;
-    }
-
-    if(window.screen.width <= 375){
-        RELOAD_LOGO.src = "../img/logo_cell_2x.png";    
-        return;
+    const RELOAD_LOGO = document.getElementById("logo");
+    if(RELOAD_LOGO){
+        logoSize();
     }
     
-    return;
-}
+    function logoSize() {
+        if(window.screen.width <= 768 && window.screen.width > 375){
+            RELOAD_LOGO.src = "./img/logo_tab_1x.png";    
+            return;
+        }
+        if(window.screen.width <= 375){
+            RELOAD_LOGO.src = "./img/logo_cell_2x.png";    
+            return;
+        }
+        return;
+    }
+
+
 
